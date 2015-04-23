df30ba5402aacd7231d20c7861df776d035acc5e
Varun Ramesh <varunramesh@live.com>
2015-04-23 03:01:28 -0700

    Can now be used as a library.

--- test.js
+++ test.js
@@ -1,4 +1,39 @@
+var todolist = require("./lib");
 var assert = require("assert");
 
 describe('findMarks', function() {
+  it('should find TODOs, NOTES, and FIXMEs', function() {
+    var result = todolist.findMarks("// TODO: This is a TODO\n// NOTE: This is a Note\n// FIXME: This is a fixme.\n");
+    assert.deepEqual(result,
+      [{
+        content: 'TODO: This is a TODO',
+        line: 0,
+        assignee: null,
+        type: 'TODOs'
+      },
+        {
+          content: 'NOTE: This is a Note',
+          line: 1,
+          assignee: null,
+          type: 'NOTEs'
+        },
+        {
+          content: 'FIXME: This is a fixme.',
+          line: 2,
+          assignee: null,
+          type: 'FIXMEs'
+      }]);
+  });
+
+  it('should parse the assignee of a task.', function() {
+    var result = todolist.findMarks("// TODO(assignee): This is a TODO assigned to bob.");
+    console.log(result);
+    assert.deepEqual(result,
+      [{
+        content: 'TODO(assignee): This is a TODO assigned to bob.',
+        line: 0,
+        assignee: 'assignee',
+        type: 'TODOs'
+      }]);
+  });
 });
