cd9cdee0affb96b2864d3914cb386d2fed3ec194
Varun Ramesh <varunramesh@live.com>
2015-05-05 16:00:28 -0700

    Case insensitive matching

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var todolist = require("./lib");
 var assert = require("assert");
 
 describe('findMarks', function() {
-  it('should find TODOs, NOTES, and FIXMEs', function() {
+  it('Find TODOs, NOTES, and FIXMEs', function() {
     var result = todolist.findMarks("// TODO: This is a TODO\n// NOTE: This is a Note\n// FIXME: This is a fixme.\n");
     assert.deepEqual(result,
       [{
@@ -25,9 +25,31 @@ describe('findMarks', function() {
       }]);
   });
 
-  it('should parse the assignee of a task.', function() {
+  it('Case-insensitive matching', function() {
+    var result = todolist.findMarks("// todo: This is a TODO\n// note: This is a Note\n// fixme: This is a fixme.\n");
+    assert.deepEqual(result,
+      [{
+        content: 'todo: This is a TODO',
+        line: 0,
+        assignee: null,
+        type: 'TODOs'
+      },
+        {
+          content: 'note: This is a Note',
+          line: 1,
+          assignee: null,
+          type: 'NOTEs'
+        },
+        {
+          content: 'fixme: This is a fixme.',
+          line: 2,
+          assignee: null,
+          type: 'FIXMEs'
+      }]);
+  });
+
+  it('Parse the assignee of a task', function() {
     var result = todolist.findMarks("// TODO(bob): This is a TODO assigned to bob.");
-    console.log(result);
     assert.deepEqual(result,
       [{
         content: 'TODO(bob): This is a TODO assigned to bob.',
